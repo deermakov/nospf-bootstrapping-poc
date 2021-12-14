f9b3d3ba5163d71208a039f772858c1058ae30cf
Ермаков Денис <DEErmakov@lan.lanit.ru>
2021-12-14 13:06:51 +0300

    Creating project

---
+++ src/test/java/ru/nospf/NospfBootstrappingPocApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.nospf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NospfBootstrappingPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
